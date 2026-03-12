b4dca3343ef63394c840294f318f74de57641dec
2022 <ivan.parysov@mail.ru>
2026-03-13 01:39:35 +0300

    NetworkHost: New view from Dialog.java

--- src/rf/ebanina/UI/Editors/Network/Controller.java
+++ src/rf/ebanina/UI/Editors/Network/Controller.java
@@ -1,48 +1,66 @@
 package rf.ebanina.UI.Editors.Network;
 
-import com.jfoenix.controls.JFXButton;
-import com.jfoenix.controls.JFXTextField;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseButton;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
 import rf.ebanina.File.Resources.ResourceManager;
 import rf.ebanina.UI.Root;
 import rf.ebanina.UI.UI.Element.ListViews.ListCells.Playlists.ListCellSimilar;
 import rf.ebanina.UI.UI.Element.ListViews.ListView;
-import rf.ebanina.ebanina.Music;
+import rf.ebanina.UI.UI.Paint.ColorProcessor;
 import rf.ebanina.ebanina.Player.Controllers.MediaProcessor;
 import rf.ebanina.ebanina.Player.Controllers.Playlist.PlayProcessor;
 import rf.ebanina.ebanina.Player.Track;
 
+import java.net.URL;
 import java.util.List;
+import java.util.ResourceBundle;
 
 import static rf.ebanina.File.Localization.LocalizationManager.getLocaleString;
 import static rf.ebanina.Network.Info.getTracks;
 
-public class Controller {
-    public AnchorPane mainPain;
-    public JFXTextField search;
-    @FXML private TextField addict;
-    @FXML private TextField download;
-    @FXML private Label numbers;
-    @FXML private JFXButton addToPlaylist;
-    @FXML private JFXButton setToPlaylist;
-    private ListView<Track> tracks;
-
+public class Controller
+        implements Initializable
+{
     @FXML
-    private void initialize() {
+    public AnchorPane mainPain;
+    @FXML
+    protected TextField search;
+    @FXML
+    protected Label addictLabel;
+    @FXML
+    protected Label searchLabel;
+    @FXML
+    protected Label downloadLabel;
+    @FXML
+    protected TextField addict;
+    @FXML
+    protected TextField download;
+    @FXML
+    protected Label numbers;
+    @FXML
+    protected Button addToPlaylist;
+    @FXML
+    protected Button setToPlaylist;
+
+    protected ListView<Track> tracks;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
         tracks = new ListView<>();
         tracks.setLayoutX(340);
         tracks.setLayoutY(20);
         tracks.setPrefWidth(500);
         tracks.setPrefHeight(560);
         tracks.setId("tracks");
-        tracks.setStyle("-fx-background-color: white; -fx-border-color: #7092be;");
 
         AnchorPane.setBottomAnchor(tracks, 20d);
         AnchorPane.setLeftAnchor(tracks, 340d);
@@ -62,14 +80,32 @@ public class Controller {
 
         addict.setText("search");
 
+        Color mainColor = ColorProcessor.core.getMainClr();
+        String hexColor = ColorProcessor.core.toHex(mainColor);
+
+        mainPain.setStyle("-fx-background-color: #1E1E1E;");
+
+        tracks.setStyle("-fx-background-color: #2D2D2D; -fx-border-color: " + hexColor + ";");
+
+        addToPlaylist.setStyle("-fx-background-color: " + hexColor + "; -fx-text-fill: white;");
+        setToPlaylist.setStyle("-fx-background-color: " + hexColor + "; -fx-text-fill: white;");
+
+        addictLabel.setStyle("-fx-font-weight: bold; -fx-text-fill: " + hexColor + ";");
+        searchLabel.setStyle("-fx-font-weight: bold; -fx-text-fill: " + hexColor + ";");
+        downloadLabel.setStyle("-fx-font-weight: bold; -fx-text-fill: " + hexColor + ";");
+        numbers.setStyle("-fx-font-weight: bold; -fx-text-fill: " + hexColor + ";");
+        download.setStyle("-fx-background-color: #333333; -fx-text-fill: white; -fx-prompt-text-fill: #AAAAAA; -jfx-unfocus-color: " + hexColor + "; -jfx-focus-color: " + hexColor + ";");
+        addict.setStyle("-fx-background-color: #333333; -fx-text-fill: white; -fx-prompt-text-fill: #AAAAAA; -jfx-unfocus-color: " + hexColor + "; -jfx-focus-color: " + hexColor + ";");
+        search.setStyle("-fx-background-color: #333333; -fx-text-fill: white; -fx-prompt-text-fill: #AAAAAA; -jfx-unfocus-color: " + hexColor + "; -jfx-focus-color: " + hexColor + ";");
+
+        numbers.setStyle("-fx-text-fill: " + hexColor + "; -fx-font-weight: bold;");
+
         download.setOnKeyPressed(keyEvent -> {
             if (keyEvent.getCode() == KeyCode.ENTER) {
                 numbers.setText("");
 
                 new Thread(() -> {
                     List<Track> res = getTracks.getTrack(download.getText(), "200", addict.getText());
-
-                    Music.mainLogger.println(res);
 
                     Platform.runLater(() -> {
                         tracks.getItems().clear();
@@ -83,25 +119,25 @@ public class Controller {
 
         tracks.setOnMouseClicked((e) -> {
             if(e.getButton() == MouseButton.PRIMARY) {
-                if(!PlayProcessor.playProcessor.isNetwork()) {
-                    PlayProcessor.playProcessor.setNetwork(true);
+                if(tracks.getSelectionModel().getSelectedItem() != null) {
+                    if (!PlayProcessor.playProcessor.isNetwork()) {
+                        PlayProcessor.playProcessor.setNetwork(true);
 
-                    PlayProcessor.playProcessor.getTracks().clear();
-                    PlayProcessor.playProcessor.getTracks().addAll(tracks.getItems());
+                        PlayProcessor.playProcessor.getTracks().clear();
+                        PlayProcessor.playProcessor.getTracks().addAll(tracks.getItems());
 
-                    PlayProcessor.playProcessor.setTrackIter(tracks.getSelectionModel().getSelectedIndex());
+                        PlayProcessor.playProcessor.setTrackIter(tracks.getSelectionModel().getSelectedIndex());
 
-                    Root.similar.getTrackListView().getItems().clear();
-                    Root.similar.getTrackListView().getItems().addAll(PlayProcessor.playProcessor.getTracks());
-                    Root.similar.getTrackListView().getSelectionModel().select(PlayProcessor.playProcessor.getTrackIter());
+                        Root.similar.getTrackListView().getItems().clear();
+                        Root.similar.getTrackListView().getItems().addAll(PlayProcessor.playProcessor.getTracks());
+                        Root.similar.getTrackListView().getSelectionModel().select(PlayProcessor.playProcessor.getTrackIter());
 
-                    Root.PlaylistHandler.playlistSimilar.clear();
-                    Root.PlaylistHandler.playlistSimilar.addAll(PlayProcessor.playProcessor.getTracks());
+                        Root.PlaylistHandler.playlistSimilar.clear();
+                        Root.PlaylistHandler.playlistSimilar.addAll(PlayProcessor.playProcessor.getTracks());
+                    }
+
+                    MediaProcessor.mediaProcessor.regenerateMediaPlayer(tracks.getSelectionModel().getSelectedItem());
                 }
-
-                MediaProcessor.mediaProcessor.regenerateMediaPlayer(tracks.getSelectionModel().getSelectedItem());
-
-                // OnlineTrack.play(tracks.getSelectionModel().getSelectedItem());
             }
         });
 
--- src/rf/ebanina/UI/Editors/Network/NetworkHost.java
+++ src/rf/ebanina/UI/Editors/Network/NetworkHost.java
@@ -1,52 +1,37 @@
 package rf.ebanina.UI.Editors.Network;
 
 import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import rf.ebanina.File.Resources.ResourceManager;
 import rf.ebanina.UI.Editors.IEditor;
 import rf.ebanina.UI.Root;
+import rf.ebanina.UI.UI.Element.AnimationDialog;
 import rf.ebanina.UI.UI.Paint.ColorProcessor;
 
-import java.awt.*;
 import java.io.IOException;
-import java.util.Map;
 
-import static rf.ebanina.File.Localization.LocalizationManager.getLocaleString;
-import static rf.ebanina.UI.Root.windowsSizes;
-import static rf.ebanina.ebanina.Music.name;
-
-public class NetworkHost implements IEditor {
+public class NetworkHost
+        implements IEditor
+{
     public static NetworkHost instance = new NetworkHost();
 
     @Override
     public void open(Stage stage) {
         try {
             Parent loader = ResourceManager.Instance.loadFXML("FXMLDownloadInetPath");
-            Scene scene = new Scene(loader);
 
-            Stage itStage = new Stage();
-            itStage.setTitle(name + " - " + getLocaleString("window_title_download_inet", "Network Host"));
-            itStage.getIcons().add(ColorProcessor.logo);
-            itStage.setScene(scene);
+            AnimationDialog dialog = new AnimationDialog(stage, Root.rootImpl.getRoot());
+            dialog.setDialogMaxSize(0.7, 0.85);
+            dialog.setTopBorder(ColorProcessor.core.getMainClr());
 
-            Map.Entry<Point, Dimension> windowSize = windowsSizes.get(Controller.class.getName());
+            VBox dialogContent = dialog.getDialogBox();
+            dialogContent.getChildren().add(loader);
 
-            if(windowSize != null) {
-                itStage.setWidth(windowSize.getValue().getWidth());
-                itStage.setHeight(windowSize.getValue().getHeight());
-                itStage.setX(windowSize.getKey().getX());
-                itStage.setY(windowSize.getKey().getY());
-            }
+            VBox.setVgrow(loader, Priority.ALWAYS);
 
-            itStage.setOnCloseRequest((e) -> windowsSizes.put(Controller.class.getName(), Map.entry(
-                    new Point((int) itStage.getX(), (int) itStage.getY()), new Dimension((int) scene.getWidth(), (int) scene.getHeight())
-            )));
-
-            itStage.setResizable(true);
-            itStage.show();
-
-            Root.rootImpl.setStageCaptionColor(itStage, ColorProcessor.core.getMainClr());
+            dialog.show();
         } catch (IOException e) {
             e.printStackTrace();
         }
