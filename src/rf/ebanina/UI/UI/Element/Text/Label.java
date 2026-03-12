01dc9122ee377ee5f94793d2e82de981df154104
2022 <ivan.parysov@mail.ru>
2026-03-12 23:33:32 +0300

    Label: Unused methods removed

--- src/rf/ebanina/UI/UI/Element/Text/Label.java
+++ src/rf/ebanina/UI/UI/Element/Text/Label.java
@@ -24,36 +24,14 @@ public class Label extends javafx.scene.control.Label {
         this(text, color, 0, 0);
     }
 
-    public void setFontSize(int size) {
-        super.setFont(ResourceManager.Instance.loadFont("main_font", size));
-    }
-
     public void replace() {
         addEventHandler(MouseEvent.MOUSE_DRAGGED, event -> setLayouts(event.getSceneX() + getPrefWidth() / 2, event.getSceneY() + getPrefHeight() / 2));
-    }
-
-    public void setAnchorEvent(javafx.scene.Node node, int[] padding) {
-        node.layoutXProperty().addListener((obs, oldVal, newVal) ->
-            setLayoutX(node.getLayoutX() + padding[0])
-        );
-
-        node.layoutYProperty().addListener((obs, oldVal, newVal) ->
-            setLayoutY(node.getLayoutY() + padding[1])
-        );
-    }
-
-    public void showExternalBounds(String webColor) {
-        // super.setStyle("-fx-border-color: " + webColor + ";");
     }
 
     public Label setColor(Color c) {
         setTextFill(c);
 
         return this;
-    }
-
-    public void updateColor(Color color) {
-        super.setTextFill(color);
     }
 
     public void setLayouts(double x, double y) {
