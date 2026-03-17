4f9849d041a92a1c07e3d4a5d74bbca36aea3f4c
2022 <ivan.parysov@mail.ru>
2026-03-17 16:50:14 +0300

    AudioHost: Fixed freeze open animation

--- src/rf/ebanina/UI/Editors/Player/AudioHost.java
+++ src/rf/ebanina/UI/Editors/Player/AudioHost.java
@@ -2,61 +2,51 @@ package rf.ebanina.UI.Editors.Player;
 
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import rf.ebanina.ebanina.Music;
-import rf.ebanina.ebanina.Player.AudioPlugins.PluginWrapper;
 import rf.ebanina.File.Resources.ResourceManager;
-import rf.ebanina.UI.Root;
-import rf.ebanina.UI.UI.Paint.ColorProcessor;
 import rf.ebanina.UI.Editors.IEditor;
+import rf.ebanina.UI.Root;
+import rf.ebanina.UI.UI.Element.AnimationDialog;
+import rf.ebanina.UI.UI.Paint.ColorProcessor;
+import rf.ebanina.ebanina.Player.AudioPlugins.PluginWrapper;
 
-import java.awt.*;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
-import static rf.ebanina.File.Localization.LocalizationManager.getLocaleString;
-import static rf.ebanina.UI.Root.windowsSizes;
-
-public class AudioHost implements IEditor {
+public class AudioHost
+        implements IEditor
+{
     public static final AudioHost instance = new AudioHost();
 
     public List<PluginWrapper> vstPlugins = new ArrayList<>();
 
     @Override
-    public void open(Stage stage) {
-        Scene scene;
-
-        Stage hostStage = new Stage();
-
-        Map.Entry<Point, Dimension> windowSize = windowsSizes.get(getClass().getName());
-
-        if(windowSize != null) {
-            hostStage.setX(windowSize.getKey().getX());
-            hostStage.setY(windowSize.getKey().getY());
-            hostStage.setWidth(windowSize.getValue().getWidth());
-            hostStage.setHeight(windowSize.getValue().getHeight());
-        }
-
+    public void open(Stage ownerStage) {
         try {
             Parent parent = ResourceManager.Instance.loadFXML("FXMLAudioHostPath");
 
-            scene = new Scene(parent);
-            scene.getStylesheets().add(ResourceManager.Instance.loadStylesheet("vsthost"));
+            AnimationDialog hostDialog = new AnimationDialog(ownerStage, Root.rootImpl.getRoot());
+            hostDialog.setDialogMaxSize(0.75, 0.75);
+            hostDialog.setTopBorder(ColorProcessor.core.getMainClr());
+
+            VBox dialogContent = hostDialog.getDialogBox();
+            dialogContent.getChildren().add(parent);
+            VBox.setVgrow(parent, Priority.ALWAYS);
+
+            Scene scene = parent.getScene();
+            if (scene != null) {
+                scene.getStylesheets().add(ResourceManager.Instance.loadStylesheet("vsthost"));
+            }
+
+            javafx.application.Platform.runLater(() -> {
+                hostDialog.show();
+                hostDialog.animationTopBorder(ColorProcessor.core.getMainClr()).play();
+            });
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
-        hostStage.setOnCloseRequest((e) -> windowsSizes.put(getClass().getName(),
-                Map.entry(new Point((int) hostStage.getX(), (int) hostStage.getY()), new Dimension((int) scene.getWindow().getWidth(), (int) scene.getWindow().getHeight()))
-        ));
-
-        hostStage.initOwner(stage);
-        hostStage.setTitle(Music.name + " - " + getLocaleString("window_title_host", "Audio-Host"));
-        hostStage.setScene(scene);
-        hostStage.show();
-
-        Root.setStageCaptionColor(hostStage, ColorProcessor.core.getMainClr());
     }
 }
--- src/rf/ebanina/UI/Editors/Player/Controller.java
+++ src/rf/ebanina/UI/Editors/Player/Controller.java
@@ -5,8 +5,10 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TabPane;
 import javafx.scene.layout.VBox;
-import rf.ebanina.ebanina.Player.Controllers.MediaProcessor;
+import javafx.scene.paint.Color;
 import rf.ebanina.File.Resources.ResourceManager;
+import rf.ebanina.UI.UI.Paint.ColorProcessor;
+import rf.ebanina.ebanina.Player.Controllers.MediaProcessor;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -28,8 +30,12 @@ public class Controller implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        main_text.setText(getLocaleString("vst_editor_main_text", "Audio-Host"));
+        Color mainColor = ColorProcessor.core.getMainClr();
+        String hexColor = ColorProcessor.core.toHex(mainColor);
 
+        main_text.setText(getLocaleString("vst_editor_main_text", "Audio-Host"));
+        main_text.setStyle("-fx-font-weight: bold; -fx-text-fill: " + hexColor + ";");
         tabPane.getStylesheets().add(ResourceManager.Instance.loadStylesheet("tabpane"));
+        tabPane.setStyle("-fx-accent-color: " + hexColor + ";");
     }
 }
