69d6c45c38e070995bc89e190e769bb78312908f
2022 <ivan.parysov@mail.ru>
2026-04-11 23:03:28 +0300

    IParseAlbumArt.java now is functional interface

--- src/rf/ebanina/Network/IParseAlbumArt.java
+++ src/rf/ebanina/Network/IParseAlbumArt.java
@@ -2,6 +2,7 @@ package rf.ebanina.Network;
 
 import javafx.scene.image.Image;
 
+@FunctionalInterface
 public interface IParseAlbumArt {
     Image getAlbumArt(String view, int height, int width, boolean isPreserveRatio, boolean isSmooth);
 }
